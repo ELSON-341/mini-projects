4ea1dc2c5e7a8aa5a3420f743c2f4cdd54199cf0
ELSON-341 <elson.sn314@gmail.com>
2024-02-16 14:52:40 -0300

    commit

--- tabuada-js/js/script.js
+++ tabuada-js/js/script.js
@@ -1,20 +1,33 @@
 // Seleção de elementos
 const multicationForm = document.querySelector('#multiplication-form')
 const numberInput = document.querySelector('#number')
-const mltiplicationIpunt = document.querySelector('#multiplicator')
-
+const multiplicationIpunt = document.querySelector('#multiplicator')
+const multiplicatorTable = document.querySelector('#multiplication-operations')
 // Funções
+const createTable = (number, multiplicatorNumber) => {
+    multiplicatorTable.innerHTML = ''
 
+    for(let i = 1; i <= multiplicatorNumber; i++){
+        const result = number * i
+
+        const templete = `<div class="row">
+            <div class="operation"> ${number} X ${i} = <div/>
+            <class="result">${result}<div/>
+            <div/>`
+    }
+}
 // eventos
 multicationForm.addEventListener("submit", (e) => {
     e.preventDefault()
 
     const multiplicationNumber = +numberInput.value
 
-    const multiplicatiorNumber = +mltiplicationIpunt.value
+    const multiplicatorNumber = +multiplicationIpunt.value
     
-    if(!multiplicationNumber || !multiplicatiorNumber) return
-
-    console.log(multiplicationNumber, multiplicatiorNumber)
+    if(!multiplicationNumber || !multiplicatorNumber) {
+        window.alert('Preenchar os campo!')
+        return
+    }
+    createTable(multiplicationNumber, multiplicatorNumber)
 
 })
